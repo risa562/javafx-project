e14fea4aed5e6411a0ced1d0f9d49d4ee486a69f
Riku Sankiaho <R1ks4@hotmail.com>
2020-04-29 14:48:14 +0300

    v0.4; documentation added for existing features

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,26 +1,38 @@
 
-package com.company;
+/**
+ * CODE/TEXT EDITOR
+ *
+ * A partially functional code editor. Still lacking most of it's planned features,
+ * but it's capable of opening, editing and saving text files already.
+ *
+ * KOODI/TEKSTIEDITORI
+ *
+ * Osittain toimiva koodieditori. Suurin osa suunnitelluista ominaisuuksista puuttuu vielä,
+ * mutta ohjelmalla voi jo avata, muokata ja tallentaa tekstitiedostoja.
+ *
+ *
+ * @author  Riku Sänkiaho
+ * @version 0.4
+ * @since   2020-04-29
+ */
+        package com.company;
 
 import com.company.util.FileHandler;
-//import java.util.logging.FileHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.*;
 import javafx.scene.layout.*;
 import javafx.application.Application;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.ResourceBundle;
-import java.util.concurrent.atomic.AtomicReference;
 
 
 public class App extends Application {
@@ -39,23 +51,32 @@ public class App extends Application {
     private TextArea textarea1;
     private String content;
     private String filePath;
+    private String emptycheck;
 
     public void stop() {
     }
 
-
-
-    //public static final StageStyle UNIFIED
     @Override
     public void start(Stage mainStage) throws IOException {
 
         FileChooser filechooser1 = new FileChooser();
         FileHandler filehandler1 = new FileHandler();
 
-        //LOCALIZATION
-
-        //Locale locale = new Locale("en", "US");
-        Locale locale = Locale.getDefault();
+        /**
+         *LOCALIZATION
+         *   The program will crash if locale is not found, hence Locale.getDefault will not work outside of finnish/english settings.
+         *TO-DO LIST:
+         *   - switch to a preset locale if default locale is not found
+         *
+         *LOKALISAATIO
+         *   Ohjelma kaatuu jos oikeaa lokaalisaatiota ei löydy, joten Locale.getDefault ei toimi muilla kuin suomen- ja
+         *   englanninkielisillä asetuksilla.
+         *TEHTÄVÄLISTA:
+         *   -vaihda lokaali valmiiksi määriteltyyn vaihtoehtoon jos oikeaa ei löydy
+         *
+         */
+        Locale locale = new Locale("en", "US");
+        //Locale locale = Locale.getDefault();
         ResourceBundle labels = ResourceBundle.getBundle("language", locale);
 
         String title = labels.getString("title");
@@ -78,24 +99,32 @@ public class App extends Application {
 
         String about2 = labels.getString("about2");
 
-        //MENUS
+        /**
+        * INTERFACE CONSTRUCTION - MENU
+        *    Create all menus and their subcategories.
+        * KÄYTTÖLIITTYMÄN RAKENNUS - VALIKKO
+        *   Luo valikot ja niiden alaluokat.
+        */
 
-        MenuBar menubar1 = new MenuBar();
-        Menu menu1 = new Menu(file);
+        MenuBar menubar1 = new MenuBar();                   //luodaan valikkopalkko
+
+        Menu menu1 = new Menu(file);                        //luodaan valikot
         Menu menu2 = new Menu(edit);
         Menu menu3 = new Menu(run);
         Menu menu4 = new Menu(about);
-        menubar1.getMenus().add(menu1);
+
+        menubar1.getMenus().add(menu1);                     //lisätään valikot valikkopalkkiin
         menubar1.getMenus().add(menu2);
         menubar1.getMenus().add(menu3);
         menubar1.getMenus().add(menu4);
-        MenuItem newfile = new MenuItem(newitem);
-        MenuItem openfile0 = new MenuItem(open);
+
+        MenuItem newfile = new MenuItem(newitem);           //luodaan valikoiden sisältö
+        MenuItem openfile = new MenuItem(open);
         MenuItem savefile = new MenuItem(save);
         MenuItem exitprog = new MenuItem(exit);
 
-        menu1.getItems().add(newfile);
-        menu1.getItems().add(openfile0);
+        menu1.getItems().add(newfile);                      //lisätään valikon sisältö valikkoon
+        menu1.getItems().add(openfile);
         menu1.getItems().add(savefile);
         menu1.getItems().add(exitprog);
 
@@ -113,106 +142,139 @@ public class App extends Application {
         MenuItem aboutthis = new MenuItem(about2);
         menu4.getItems().add(aboutthis);
 
-        Clipboard clipboard1 = Clipboard.getSystemClipboard();
-        ClipboardContent content1 = new ClipboardContent();
-        content1.putString("");
-        content1.putHtml("<b>test</b>");
-
-        //ABOUT
+        /**
+        *INTERFACE CONSTRUCTION - ABOUT PAGE
+        *KÄYTTÖLIITTYMÄN RAKENNUS - OHJELMAN TIEDOT -SIVU
+        */
 
         Alert aboutme = new Alert(Alert.AlertType.INFORMATION);
         aboutme.setTitle("About this software");
-        aboutme.setHeaderText("JavaFX Code Editor v0.1");
-        aboutme.setContentText("This page is currently a work in progress, come back later!");
+        aboutme.setHeaderText("JavaFX Code Editor 3000 v0.4");
+        aboutme.setContentText("A crude code/text editor by Riku Sänkiaho!");
 
-        //
-        Button clear = new Button("clear");
-        TextArea textarea1 = new TextArea();
-        textarea1.setWrapText(true);
+        /**
+        *INTERFACE CONSTRUCTION - TOOLS AND WORK AREA
+         * Create the necessary interface elements and connect them.
+        *TO-DO LIST:
+         *   -finish adding the font menu and add font changing functionality
+         *
+        *KÄYTTÖLIITTYMÄN RAKENNUS - TYÖKALUT JA TYÖALUE
+         * Luo käyttöliittymän eri elementit ja yhdistä ne toimivaksi kokonaisuudeksi.
+        *TEHTÄVÄLISTA:
+         *    -lisää fonttivalikko ja fontin vaihtaminen tekstialueella
+        */
 
-        //KEYS & HOTKEYS
+        Button clear = new Button("clear");               //luodaan clear-nappi
+        TextArea textarea1 = new TextArea();                //luodaan tekstialue
+        textarea1.setFont(Font.font ("Verdana", 14));  //valitaan alustava fontti tekstialueelle
+        textarea1.setWrapText(true);                        //tekstialue rajataan niin että ylimenevä teksti siirtyy seuraavalle riville
 
+        ColorPicker colorpicker1 = new ColorPicker();
+        TextField fontsize = new TextField();
+        fontsize.setPrefColumnCount(3);                     //fonttivalikon pohjaa, ei valmis
+
+        BorderPane root = new BorderPane(textarea1);        //luodaan pohja
+        root.setCenter(textarea1);                          //lisätään pohjaan tekstialue
+        ToolBar toolbar1 = new ToolBar(clear,colorpicker1); //luodaan valikkopalkki ja liitetään siihen työkalut
+        VBox vbox1 = new VBox(menubar1, toolbar1);          //luodaan yläkomponentti ja liitetään siihen molemmat valikkopalkit
+        root.setTop(vbox1);                                 //lisätään yläkomponentti pohjaan
+
+        /**
+         *INTERFACE INITIALIZATION
+         *KÄYTTÖLIITTYMÄN ALUSTUS
+         */
+
+        Scene scene = new Scene(root, 800, 600);      //luodaan scene/ikkuna
+        mainStage.setTitle("Editor");                       //nimetään ikkuna
+        mainStage.initStyle(StageStyle.UNIFIED);            //valitaan tyyli
+        mainStage.setScene(scene);
+        mainStage.show();                                   //näytetään ikkuna
+        mainStage.centerOnScreen();                         //siirretään ikkuna ruudun keskelle
+
+        /**
+         *KEYS & HOTKEY SETUP
+         *TO-DO LIST:
+          *     -remember to add hotkeys to new features
+         *
+         *NÄPPÄIMET JA PIKANÄPPÄIMET
+         *TEHTÄVÄLISTA:
+         *      -muista lisätä pikanäppäimet uusille toiminnoille
+         */
+
+        textarea1.setOnKeyPressed(keyEvent -> {                         //tabulaattorin muuntaminen välilyönneiksi
+            if(keyEvent.getCode() == KeyCode.TAB) {                     //jos TABia painetaan
+                int index1 = textarea1.getCaretPosition();              //tarkastetaan sijainti
+                textarea1.replaceText(index1-1, index1, "     ");  //laitetaan sijalle välilyönnit
+            }
+        });
+
+        newfile.setAccelerator(new KeyCodeCombination(KeyCode.N, KeyCombination.SHORTCUT_DOWN));    //pikanäppäimet
+        openfile.setAccelerator(new KeyCodeCombination(KeyCode.O, KeyCombination.SHORTCUT_DOWN));
+        savefile.setAccelerator(new KeyCodeCombination(KeyCode.S, KeyCombination.SHORTCUT_DOWN));
         cuttext.setAccelerator(new KeyCodeCombination(KeyCode.X, KeyCombination.SHORTCUT_DOWN));
         copytext.setAccelerator(new KeyCodeCombination(KeyCode.C, KeyCombination.SHORTCUT_DOWN));
         pastetext.setAccelerator(new KeyCodeCombination(KeyCode.V, KeyCombination.SHORTCUT_DOWN));
 
-        textarea1.setOnKeyPressed(keyEvent -> {
-            if(keyEvent.getCode() == KeyCode.TAB) {
-                int index1 = textarea1.getCaretPosition();
-                textarea1.replaceText(index1-1, index1, "     ");
-            }
+
+
+        /**
+         *ACTIONS
+         *TO-DO LIST:
+         *     -add "Save To" feature
+         *     -move certain features such as saving and opening to their own class to clean up the code.
+         *     -add confirmation to clear button
+         *     -reset color and font on new file
+         *
+         *TOIMINNOT
+         *TEHTÄVÄLISTA:
+         *     -lisää "Tallenna Nimellä" toiminto
+         *     -siirrä toiminnot kuten tallennus ja avaaminen omiin luokkiinsa koodin selventämiseksi.
+         *     -lisää clear-nappiin varmistus
+         *     -resetoi väri ja fontti uudessa tiedostossa
+         */
+
+        colorpicker1.setOnAction(actionEvent -> {                           //värivalikkoa käytettäessä
+            Color value = colorpicker1.getValue();                          //otetaan väriarvo (HEX)
+            //System.out.println(value);
+            textarea1.setStyle(                                             //muutetaan tekstialueen tekstin väriä
+                    "-fx-text-fill: " + toRGBString(value) + ";");          //väriarvo lähetetään muunnettavaksi
         });
 
+        clear.setOnAction(actionEvent ->  textarea1.clear());               //clear:ia painettaessa tekstialue puhdistuu
+        cuttext.setOnAction(actionEvent -> textarea1.cut());                //leikkaa
+        copytext.setOnAction(actionEvent -> textarea1.copy());              //kopioi
+        pastetext.setOnAction(actionEvent -> textarea1.paste());            //liitä
 
-        //TOOLS
-
-        ColorPicker colorpicker1 = new ColorPicker();
-
-        TextField fontsize = new TextField();
-        fontsize.setPrefColumnCount(3);
-
-        //INTERFACE SET UP
-
-        BorderPane root = new BorderPane(textarea1);
-        root.setCenter(textarea1);
-        ToolBar toolbar1 = new ToolBar(clear,colorpicker1);
-        VBox vbox1 = new VBox(menubar1, toolbar1);
-        root.setTop(vbox1);
-
-        //INTERFACE INITIALIZATION
-
-        Scene scene = new Scene(root, 800, 600);
-        mainStage.setTitle("Editor");
-        mainStage.initStyle(StageStyle.UNIFIED);
-        mainStage.setScene(scene);
-        mainStage.show();
-        mainStage.centerOnScreen();
-
-        //ACTIONS
-
-        colorpicker1.setOnAction(actionEvent -> {
-            Color value = colorpicker1.getValue();
-        });
-
-        clear.setOnAction(actionEvent -> textarea1.setText(""));
-
-        cuttext.setOnAction(actionEvent -> textarea1.cut());;
-        copytext.setOnAction(actionEvent -> textarea1.copy());
-        pastetext.setOnAction(actionEvent -> textarea1.paste());
-
-        newfile.setOnAction(actionEvent -> {
-
-            String emptycheck=textarea1.getText().trim();
-            if(!emptycheck.equals("")) {
+        newfile.setOnAction(actionEvent -> {                                //uuden tiedoston luonti
+            emptycheck=textarea1.getText().trim();                          //tarkastetaan onko tekstialue tyhjä
+            if(!emptycheck.equals("")) {                                    //jos ei, varmistetaan toiminto
                 Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
                 alert1.setTitle("Confirmation Dialog");
                 alert1.setHeaderText("Confirm creating a new file");
                 alert1.setContentText("Are you sure you want to create a new file? Unsaved changes to the current file will be lost!");
-                alert1.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
-                Optional<ButtonType> result = alert1.showAndWait();
-                if (result.get() == ButtonType.OK) {
-                    textarea1.setText("");
+                alert1.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);  //alert-ikkunan koko tarpeeksi isoksi
+                Optional<ButtonType> result = alert1.showAndWait();          //odotetaan vastausta
+                if (result.get() == ButtonType.OK) {                         //jos OK
+                    textarea1.setText("");                                   //tyhjennetään tekstialue
                 }
             }
                 });
 
-        openfile0.setOnAction(actionEvent -> {
-
-
-            String emptycheck=textarea1.getText().trim();
-            if(emptycheck.equals("")) {
-            File selectedFile = filechooser1.showOpenDialog(mainStage);
+        openfile.setOnAction(actionEvent -> {                                  //avataan tiedosto
+            String emptycheck=textarea1.getText().trim();                       //tarkistetaan onko tyhjä jne.
+            if(emptycheck.equals("")) {                                         //jos on:
+            File selectedFile = filechooser1.showOpenDialog(mainStage);         //avataan valintaikkuna
                 try {
-                    filehandler1.setFilePath(selectedFile.getAbsolutePath());
-                    content = filehandler1.openfile();
-                    textarea1.setText(content);
-                    filePath = selectedFile.getAbsolutePath();
+                    filehandler1.setFilePath(selectedFile.getAbsolutePath());   //otetaan tiedoston polku
+                    content = filehandler1.openfile();                          //käytetään FileHandleria avaamiseen
+                    textarea1.setText(content);                                 //laitetaan sisältö tekstialueelle
+                    //filePath = selectedFile.getAbsolutePath();
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
             } else {
-                Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);
-                alert1.setTitle("Confirmation Dialog");
+                Alert alert1 = new Alert(Alert.AlertType.CONFIRMATION);         //varoitetaan jos tiedosto ei ole tyhjä
+                alert1.setTitle("Confirmation Dialog");                         //muuten sama kuin ylempänä
                 alert1.setHeaderText("Confirm opening a new file");
                 alert1.setContentText("Are you sure you want to open a new file? Unsaved changes to the current file will be lost!");
                 alert1.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
@@ -231,9 +293,9 @@ public class App extends Application {
             }
                 });
 
-        savefile.setOnAction(actionEvent -> {
+        savefile.setOnAction(actionEvent -> {                               //tallennus
                     try {
-                        filehandler1.savefile(textarea1.getText());
+                        filehandler1.savefile(textarea1.getText());         //tallennetaan FileHandlerin avulla
                     } catch (Exception e) {
                         System.out.println(e.getMessage());
                     }
@@ -248,6 +310,26 @@ public class App extends Application {
         exitprog.setOnAction(actionEvent -> System.exit(0));
 
         //clipboard1.setContent(content);
+
+    }
+
+    /**
+     *COLOR VALUE TRANSLATOR
+     *Receives color values from the color picker (0-1 for red, green and blue) and translates them to a form
+     *that setStyle accepts (0-255 for red, green and blue respectively).
+
+     *VÄRIARVON KÄÄNTÄJÄ
+     *ottaa vastaan väriarvoja ja muuttaa ne sopivaan muotoon setStyle komennolle. Heksadesimaaliarvoista saadaan
+     *värin komponenttien arvot (0-1 punaiselle, vihreälle ja siniselle) getRed/getGreen/getBlue komennolla.
+     *Nämä arvot muunnetaan RGB-muotoon kertomalla ne 255:llä.
+     */
+
+    private String toRGBString(Color c) {
+        return "rgb("
+                + (255*c.getRed())
+                + "," + (255*c.getGreen())
+                + "," + (255*c.getBlue())
+                + ")";
 
     }
 
